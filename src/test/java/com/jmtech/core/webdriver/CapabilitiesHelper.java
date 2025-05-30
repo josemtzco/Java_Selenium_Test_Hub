b082d2a224d7624ce9d926b3aea86317d3c0064a
Jose Martinez <76101058+josemtzco@users.noreply.github.com>
2025-05-30 00:41:07 -0500

    Changed the start maximized to default resolution
    I found an issue where the container at work got smaller resolution and some test were failing due to that. with this they dont fail

--- src/test/java/com/jmtech/core/webdriver/CapabilitiesHelper.java
+++ src/test/java/com/jmtech/core/webdriver/CapabilitiesHelper.java
@@ -1,18 +1,17 @@
 package com.jmtech.core.webdriver;
 
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class CapabilitiesHelper {
 
     public static ChromeOptions getChromeCapabilities() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized", "--disable-notifications");
 
         // Maximize the window and disable notifications
-        options.addArguments("--start-maximized", "--disable-notifications");
+        options.addArguments("--window-size=1920,1080", "--disable-notifications");
 
         // Disable extensions to avoid interference
         options.addArguments("--disable-extensions");
